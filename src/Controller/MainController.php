4ab045c1f1b22f607690523ab6be9a08049516f3
Mihcał robak <kluskaaaaa@o2.pl>
2025-03-07 11:29:50 +0100

    Usuniecie narzedzia do poprawy skadni po wersja php była zbyt nowa. dodanie twiga. moment przed instalacja debugera

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,16 +2,27 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function homepage():Response
     {
-        return new Response('<strong>Starshop</strong>:your monopoly-busting option for Starship parts!');
+        $myShip=[
+            'name'=> 'USS LeafyCruiser (NCC-0001)',
+            'class' => 'Garden',
+            'captain' => 'Jeanm-Luc Pickles',
+            'status' => 'under construction',
+        ];
+
+
+        $starshipCount = 457;
+        //return new Response('<strong>Starshop</strong>:your monopoly-busting option for Starship parts!');
+        return $this->render('main/homepage.html.twig',['numberOfStarships' => $starshipCount, 'myShip' => $myShip]);
     }
 
 }
